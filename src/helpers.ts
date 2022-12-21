ebfacff354d305689d3061890d16fb2f67a6ce3c
Agent_RBY_ <mrget112@gmail.com>
2022-12-22 01:40:39 +0200

    Change startsWith to Regex
    Took 8 minutes

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,7 +1,9 @@
+const LINK_REGEX = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/gi;
+
 export function capitalize(text: string): string {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
 export function isStartsWithLink(text: string): boolean {
-  return text.startsWith("http://") || text.startsWith("https://");
+  return LINK_REGEX.test(text);
 }
