b0d761b191c3c9ea3efa7d7a687f7726c383d210
Agent_RBY_ <mrget112@gmail.com>
2022-12-22 01:41:53 +0200

    Prettier all
    Took 1 minute

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,5 @@
-const LINK_REGEX = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/gi;
+const LINK_REGEX =
+  /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/gi;
 
 export function capitalize(text: string): string {
   return text.charAt(0).toUpperCase() + text.slice(1);
