943011043fc97ad2448d05c1783f797bfa00b002
Agent_RBY_ <mrget112@gmail.com>
2022-12-22 01:51:43 +0200

    Use common modules instead searching
    Took 10 minutes

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,20 @@
-import { Injector, webpack } from "replugged";
+import { Injector, common } from "replugged";
 import { capitalize, isStartsWithLink } from "./helpers";
 
 const inject = new Injector();
 
-interface SendMessageEvent {
-  content: string;
-  invalidEmojis: string[];
-  tts: boolean;
-  validNonShortcutEmojis: string[];
-}
+export function start(): void {
+  inject.after(common.messages, "sendMessage", (args) => {
+    const { content } = args[1];
 
-export async function start(): Promise<void> {
-  const sendMessageMod = await webpack.waitForModule<{
-    sendMessage: (channelId: string, message: SendMessageEvent) => void;
-  }>(webpack.filters.byProps("sendMessage"));
-
-  if (sendMessageMod) {
-    inject.after(sendMessageMod, "sendMessage", (args) => {
-      const { content } = args[1];
-
-      if (isStartsWithLink(content)) {
-        return args;
-      }
-
-      args[1].content = capitalize(content);
-
+    if (isStartsWithLink(content)) {
       return args;
-    });
-  }
+    }
+
+    args[1].content = capitalize(content);
+
+    return args;
+  });
 }
 
 export function stop(): void {
