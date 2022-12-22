867fd9243f8b3ad87da75c6198dc996ead2e1905
Agent_RBY_ <mrget112@gmail.com>
2022-12-22 02:07:17 +0200

    Make link-regex work only on start
    Took 8 minutes

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,11 @@
 const LINK_REGEX =
-  /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/gi;
+  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/i;
 
 export function capitalize(text: string): string {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
 export function isStartsWithLink(text: string): boolean {
+  console.log(text);
   return LINK_REGEX.test(text);
 }
