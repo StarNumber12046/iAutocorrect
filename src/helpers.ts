9dfb03c3c8fcd79b89b7f4232a565b27600bd9b6
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2023-02-12 10:45:29 +0100

    make it an iAutocorrect ig

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ const LINK_REGEX =
   /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/i;
 
 export function capitalize(text: string): string {
-  return text.charAt(0).toUpperCase() + text.slice(1);
+  return "i" + text.charAt(0).toUpperCase() + text.slice(1);
 }
 
 export function isStartsWithLink(text: string): boolean {
