5e63ae5b2b60987e0974991c55eb53a55faec53b
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2023-02-12 11:29:38 +0100

    fixed weird characters && mentions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,11 @@
 const LINK_REGEX =
   /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/i;
-
+const MENTION_REGEX = /(<(@|#)(\d)+>)/
 export function capitalize(text: string): string {
-  return "i" + text.charAt(0).toUpperCase() + text.slice(1);
+  return `i${  text.charAt(0).toUpperCase()  }${text.slice(1)}`;
 }
 
 export function isStartsWithLink(text: string): boolean {
   console.log(text);
-  return LINK_REGEX.test(text);
+  return LINK_REGEX.test(text) || MENTION_REGEX.test(text);
 }
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,17 @@ export function start(): void {
     console.log(content)
     const words = content.split(" ")
     for (let j of words) {
+      
       if (!isStartsWithLink(j)) {
-        words[words.indexOf(j)] = capitalize(j)
+        if (/\W/.test(j.substring(0,1))) {
+          const e = j.substring(1)
+          console.log(e)
+          console.log(j)
+          words[words.indexOf(j)] = `${j.substring(0,1)}${capitalize(e)}`
+        }
+        else {
+          words[words.indexOf(j)] = capitalize(j)
+        }
       }
     }
     
